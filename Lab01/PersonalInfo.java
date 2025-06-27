5c92e99e85b58593c84c51614b3b70e4bbf3097e
Kowshik <xhkaushik890@gmail.com>
2025-06-27 10:40:15 +0600

    Update PersonalInfo.java

--- Lab01/PersonalInfo.java
+++ Lab01/PersonalInfo.java
@@ -1,20 +1,17 @@
-public class StudentInfo {
-public static void main(String[] args) {
-String name = "Kowshik shil";
-String studentID = "E243013";
-int age = 20;
-String email = "kowshik@example.com";
-String phone = "01910847787";
-String address = "Fatikchari, Chittagong";
-printStudentInfo(name, studentID, age, email, phone, address);
+
+public class PersonalInfo {
+    public static void main( String[] args){
+        String name = "Kowshik";
+        String id = "E243013";
+        int age = 19;
+        int number = 1910847822;
+        String gmail = "xhkaushik890@gmail.com";
+
+        System.out.println("Name :"+ name);
+        System.out.println("ID :"+ id);
+        System.out.println("Age :"+ age);
+        System.out.println("Number :"+ number);
+        System.out.println("Gmail :"+ gmail);
+    }
 }
-public static void printStudentInfo(String name, String studentID, int age, String email, String phone, String address) {
-System.out.println("Student Information:");
-System.out.println("Name: " + name);
-System.out.println("Student ID: " + studentID);
-System.out.println("Age: " + age);
-System.out.println("Email: " + email);
-System.out.println("Phone: " + phone);
-System.out.println("Address: " + address);
-}
-}
+
